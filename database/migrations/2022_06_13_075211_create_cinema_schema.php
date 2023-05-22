fcf8fd9b7ff94ed3630cceafe82f85effb9acc3d
Muhammad Ahmad <m.ahmad.awan88@gmail.com>
2023-05-22 21:52:27 +0500

    migration task

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -6,37 +6,99 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCinemaSchema extends Migration
 {
-    /** ToDo: Create a migration that creates all tables for the following user stories
-
-    For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
-    To not introduce additional complexity, please consider only one cinema.
-
-    Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
-
-    ## User Stories
-
-     **Movie exploration**
-     * As a user I want to see which films can be watched and at what times
-     * As a user I want to only see the shows which are not booked out
-
-     **Show administration**
-     * As a cinema owner I want to run different films at different times
-     * As a cinema owner I want to run multiple films at the same time in different showrooms
-
-     **Pricing**
-     * As a cinema owner I want to get paid differently per show
-     * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
-
-     **Seating**
-     * As a user I want to book a seat
-     * As a user I want to book a vip seat/couple seat/super vip/whatever
-     * As a user I want to see which seats are still available
-     * As a user I want to know where I'm sitting on my ticket
-     * As a cinema owner I dont want to configure the seating for every show
-     */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        // throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        
+        // Table for movies
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('movie_name');
+            $table->text('movie_description')->nullable();
+            $table->timestamps();
+        });
+
+         // Table for showrooms
+         Schema::create('showrooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('room_name');
+            $table->timestamps();
+        });
+
+        // Table for showtimes
+        Schema::create('showtimes', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('start_at');
+
+            $table->unsignedBigInteger('movie_id');
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+
+        // Bridge table for showtimes and showrooms
+        Schema::create('showtime_showroom', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('showtime_id');
+            $table->foreign('showtime_id')->references('id')->on('showtimes')->onDelete('cascade');
+
+            $table->unsignedBigInteger('showroom_id');
+            $table->foreign('showroom_id')->references('id')->on('showrooms')->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+
+        // seat types
+        Schema::create('seat_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->decimal('seat_premium_percentage', 6, 2);
+
+            $table->timestamps();
+        });
+
+        // Table for seats
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('showtime_id');
+            $table->foreign('showtime_id')->references('id')->on('showtimes')->onDelete('cascade');
+
+            $table->unsignedBigInteger('seat_type_id');
+            $table->foreign('seat_type_id')->references('id')->on('seat_types')->onDelete('cascade');
+
+            $table->string('row_no');
+            $table->string('seat_no');
+
+            $table->timestamps();
+        });
+        
+        // Table for bookings
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('showtime_id');
+            $table->foreign('showtime_id')->references('id')->on('showtimes')->onDelete('cascade');
+
+            $table->unsignedBigInteger('seat_id');
+            $table->foreign('seat_id')->references('id')->on('seats')->onDelete('cascade');
+
+            $table->string('customer_name');
+
+            $table->timestamps();
+        });
+
+        // Table for prices
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('showtime_id');
+            $table->foreign('showtime_id')->references('id')->on('showtimes')->onDelete('cascade');
+
+            $table->unsignedBigInteger('seat_type_id');
+            $table->foreign('seat_type_id')->references('id')->on('seat_types')->onDelete('cascade');
+
+            $table->decimal('price', 6, 2);
+
+            $table->timestamps();
+        });
     }
 
     /**
@@ -44,7 +106,14 @@ class CreateCinemaSchema extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
+        Schema::dropIfExists('movies');
+        Schema::dropIfExists('showtimes');
+        Schema::dropIfExists('showrooms');
+        Schema::dropIfExists('showtime_showroom');
+        Schema::dropIfExists('seat_types');
+        Schema::dropIfExists('seats');
+        Schema::dropIfExists('bookings');
+        Schema::dropIfExists('prices');
     }
 }
